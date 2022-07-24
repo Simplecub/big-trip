6c8cecb4e3cf5fd0fdc8f1451e4c7df21c33d287
Simplecub <exdimail@gmail.com>
2022-07-24 21:20:02 +0400

    разбивка-1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import NewButtonView from "./view/new-button.js";
 import {render} from "./render.js";
 import BoardPresenter from './presenter/board-presenter'
-const siteMainEl = document.querySelector('.page-main')
+import CreateFilterView from './view/filter.js';
+import CreateNewPointView from './view/new-point.js';
+import CreatePointNotDestView from './view/new-point-without-destination.js';
+import CreatePointNotOffersView from './view/new-point-without-offers.js';
+import CreateEditFormView from './view/edit-form.js';
+const siteHeaderEl = document.querySelector('.trip-controls__filters')
+const sitePageBodyEl = document.querySelector('.trip-events')
 
-render(new NewButtonView(), siteMainEl)
+//render(new NewButtonView(), siteMainEl)
 
+render(new CreateFilterView(), siteHeaderEl)
 
 const boardPresenter = new BoardPresenter()
-boardPresenter.init(siteMainEl)
+
+boardPresenter.init(sitePageBodyEl)
+
+render(new CreateNewPointView(), sitePageBodyEl)
+render(new CreatePointNotDestView(), sitePageBodyEl)
+render(new CreatePointNotOffersView(), sitePageBodyEl)
 
 console.log('fgfg+jnkjnjk')
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,16 +1,17 @@
 import NewButtonView from '../view/new-button.js'
+import CreateSortView from '../view/sort.js';
 import {render} from "../render.js";
 
 export default class BoardPresenter {
   boardComponent = new NewButtonView();
-  //taskListComponent = new TaskListView();
+  sortComponent = new CreateSortView();
 
   init = (boardContainer) => {
     this.boarContainer = boardContainer;
 
-   render(this.boardComponent, this.boarContainer)
-  //  render(new SortView(), this.boardComponent.getElement())
- //   render(this.taskListComponent, this.boardComponent.getElement());
+   render(this.sortComponent, this.boarContainer)
+
+// render(this.sortComponent, this.boardComponent.getElement());
  //   render(new TaskEditView(), this.taskListComponent.getElement());
 
     for (let i = 0; i <3; i++){
