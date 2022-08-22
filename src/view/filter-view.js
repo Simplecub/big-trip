ef373307128cdb88704bcda64496a5e4c78aeb0e
Simplecub <exdimail@gmail.com>
2022-08-22 15:02:10 +0400

    step-4 add generate-filter

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,32 +1,29 @@
-import {createElement} from "../render.js";
+import {createElement} from '../render.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
 
-
-
 const createFilterItemTemplate = (filter, isChecked) => {
-  const {name, count} = filter
+  const {name, count} = filter;
   return (`
    <div class="trip-filters__filter">
                   <input id="filter-${name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name}"
-                  ${isChecked ? 'checked': ''}
-                  ${count === 0 ? 'disabled': ''}>
-                  <label class="trip-filters__filter-label" for="filter-${name}">${name}+grg</label>
+                  ${isChecked ? 'checked' : ''}
+                  ${count === 0 ? 'disabled' : ''}>
+                  <label class="trip-filters__filter-label" for="filter-${name}">${name}+${count}</label>
                 </div>
-  `)
-}
+  `);
+};
 
 const createFilterTemplate = (filterItem) => {
   const filtersItemsTemplate = filterItem
     .map((filter, index) => createFilterItemTemplate(filter, index === 0))
-    .join('')
+    .join('');
 
   return `<form class="trip-filters" action="#" method="get">
 ${filtersItemsTemplate}
   <button class="visually-hidden" type="submit">Accept filter</button>
-              </form>
-  `
-}
+              </form> `;
+};
 
 
 /*
@@ -55,13 +52,15 @@ const createFilter = () => (
  */
 
 export default class CreateFilterView extends AbstractView {
-  #filters = null
+  #filters = null;
+
   constructor(filters) {
     super();
-this.#filters = filters
+    this.#filters = filters;
   }
+
   get template() {
-    return createFilterTemplate(this.#filters)
+    return createFilterTemplate(this.#filters);
   }
 }
 /*
